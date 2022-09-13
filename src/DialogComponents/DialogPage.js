c093fbf7e4596a3362600adccf7e10145f926cdf
MateoGonzalez <mateogonzalez132017@gmail.com>
2022-09-13 17:40:43 -0300

    .

--- src/DialogComponents/DialogPage.js
+++ src/DialogComponents/DialogPage.js
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogTitle, Grid, TextField } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogTitle, Grid, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import data from "../data.json"
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,19 +6,28 @@ import CloseIcon from '@mui/icons-material/Close';
 export default function DialogPage(socio) {
     socio = socio.socio
     return (
-        <div>
+        <div style={{justifyContent: "center"}}>
             <DialogTitle style={{ top: "2px", left:"2px"}}>{socio.nombre} {socio.apellido}</DialogTitle>
-            <Grid container>
+            <div style={{height: "500px", width: "600px", backgroundColor: "#2e2c2c"}}>
+
+            </div>
+            {/* <Grid container rowSpacing={2} direction="column" >
+
+            
 
                 <Grid item >
-                    
+                    <Typography>{socio.dni}</Typography>
+                </Grid>
+
+                <Grid item >
+                    <Typography>{socio.fecha}</Typography>
                 </Grid>
 
             </Grid>
 
             <DialogActions>
                 <Button style={{ background: "black", color: "white", marginRight: 20 }}>Send</Button>
-            </DialogActions>
+            </DialogActions> */}
         </div>
 
     )
